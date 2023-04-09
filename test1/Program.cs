1187e09fcb8bfd8b45bb795c48133f2bed3c3900
artem <artem.repnikov.g@.gmail.com>
2023-04-09 12:42:14 +0300

    первая программа

---
+++ test1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите свое имя: ");
+var name = Console.ReadLine(); // вводим имя
+Console.WriteLine($"Привет {name}");
