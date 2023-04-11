a9f3742e5a4b6cf8b519aadea06ca6c0b9bba2e6
artem <artem.repnikov.g@.gmail.com>
2023-04-11 10:44:11 +0300

    save gitignore in test2

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+
+string s = Console.ReadLine();
+int i;
+
+if (int.TryParse(s, out i)) {
+System.Console.WriteLine($"Квадрат числа {i} = {Math.Pow(i, 2)}");
+}
+else{
+System.Console.WriteLine("Упс");
+}
--- test1/Program.cs
+++ test1/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Hello, World!");
 Console.Write("Введите свое имя: ");
 var name = Console.ReadLine(); // вводим имя
 Console.WriteLine($"Привет {name}");
+
