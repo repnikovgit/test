97aee9fea262727ccadcea3d14c6bcfbf3f7a4d2
artem <artem.repnikov.g@.gmail.com>
2023-04-11 10:17:08 +0300

    save1

---
+++ test2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+{
+    Console.WriteLine("Первый блок");
+    {
+        Console.WriteLine("Второй блок");
+    }
+}
+*/
+
+/*
+{
+    Console.WriteLine("Первый блок");
+    Console.WriteLine("Второй блок");
+}
+*/
+
+/*
+Console.Write("Введите свое имя: ");
+var name = Console.ReadLine(); // вводим имя
+Console.WriteLine($"Привет {name}"); // Выводим имя на консоль
+*/
+
+/*
+string name = "Tom"; // определяем переменную и инициализируем ее
+Console.WriteLine (name); // Tom
+name = "Bob"; // меняем значение переменной
+Console.WriteLine (name);  // Bob
+*/
+
+string name = "Tom";
+int age = 33;
+bool isEmployed = false;
+double weight = 78.65;
+
+Console.WriteLine($"Имя: {name}");
+Console.WriteLine($"Возраст: {age}");
+Console.WriteLine($"Вес: {weight}");
+Console.WriteLine($"Работает: {isEmployed}");
+
+Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {weight}м");
+
