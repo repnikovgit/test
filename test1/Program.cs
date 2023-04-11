58be8041a1bc3b796030e4e181e441be06b0ce92
artem <artem.repnikov.g@.gmail.com>
2023-04-11 18:35:26 +0300

    Удалил ненужные файлы и сохраняю изменения

--- test1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.Write("Введите свое имя: ");
-var name = Console.ReadLine(); // вводим имя
-Console.WriteLine($"Привет {name}");
-
