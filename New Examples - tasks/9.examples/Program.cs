de26d6883097860b7163fc82b9b5ce9ee2cec4a9
artem <artem.repnikov.g@.gmail.com>
2023-04-17 16:17:56 +0300

    save 9.examples

---
+++ New Examples - tasks/9.examples/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+System.Console.WriteLine("Введите второе число: ");
+string s = Console.ReadLine();
+string d = Console.ReadLine();
+int i = int.Parse(s);
+int j = int.Parse(s);
+
+if (i == Math.Pow(j,2)){
+    System.Console.WriteLine("good");
+}
+else{
+    System.Console.WriteLine("bad");
+}
