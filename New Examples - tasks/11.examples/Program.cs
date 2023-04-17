648eba6486aa63b69faf16c9e3e5711e3b092dc7
artem <artem.repnikov.g@.gmail.com>
2023-04-17 16:24:28 +0300

    save 11.examples

---
+++ New Examples - tasks/11.examples/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 5;
+int c = 9;
+int d = 18;
+int e = 21;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
