edc929d6fe5caee99fab94f922fd424444f672e7
artem <artem.repnikov.g@.gmail.com>
2023-05-21 21:24:10 +0300

    test

--- New Examples - tasks/9.examples/Program.cs
+++ New Examples - tasks/9.examples/Program.cs
@@ -1,4 +1,6 @@
-﻿System.Console.WriteLine("Введите первое число: ");
+﻿/* Напишите программу, которая на вход принимает 2 числа и проверяет, является ли первое квадратом второго */
+
+System.Console.WriteLine("Введите первое число: ");
 System.Console.WriteLine("Введите второе число: ");
 string s = Console.ReadLine();
 string d = Console.ReadLine();
