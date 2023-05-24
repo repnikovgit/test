5d9fa1c108e7185af51c5641446054f57bb9a3a8
artem <artem.repnikov.g@.gmail.com>
2023-05-24 19:31:17 +0300

    test

---
+++ homework2 c#/task 1 - sem 2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+// int num = new Random().Next(100, 1000);
+
+Console.Clear();
+System.Console.WriteLine("Введите N: ");
+int num = int.Parse(Console.ReadLine());
+int num1 = num%100;
+int num2 = num1/10;
+if (num >=100 && num < 1000)
+{
+System.Console.WriteLine($"{num2}");
+}
+else
+{
+    System.Console.WriteLine("Это не трехзначное число");
+}
+
+//Console.WriteLine($"{num} -> {num%100}");
