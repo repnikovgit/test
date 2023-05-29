c78b7f7df80cfaf10ad4b37e81725aa27f01caf5
artem <artem.repnikov.g@.gmail.com>
2023-05-30 00:00:29 +0300

    test

--- homework2 c#/task 3 - sem 2/Program.cs
+++ homework2 c#/task 3 - sem 2/Program.cs
@@ -4,21 +4,35 @@
 32679 -> 6 */
 
 System.Console.WriteLine("Введите N: ");
-int s = int.Parse(Console.ReadLine());
+int num = new Random().Next(10, 100000);
+int num = int.Parse(Console.ReadLine());
+int s;
+while (s > 100 && s <1000);
+System.Console.WriteLine(s%10);
+while (s > 999);
+System.Console.WriteLine(s/10);
+if (s < 999)
+{
+    System.Console.WriteLine(s % 10);
+}
+else{
+    System.Console.WriteLine(bad);
+}
 
 //if (s % 2 == 0){ // остаток от деления на 10
-if (s >=100 && s < 1000)
+/*if (s >=100 && s < 1000)
 {
     Console.WriteLine($"{s} -> {s%10}");
 }
 /*else{
     System.Console.WriteLine("bad");
 }*/
-
+/*
 if (s < 100)
 {
     Console.WriteLine($"Третьей цифры нет");
 }
+*/
 
 
 //Console.WriteLine($"{num} -> {num/100}{num%10}");
